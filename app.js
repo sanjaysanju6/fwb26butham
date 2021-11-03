686b949dce281262162c99c8d1e50cc0bca00a85
sanjaysanju6 <sanjaykumarbutham@gmail.com>
2021-11-03 16:19:22 -0500

    added addmods page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helicopterRouter = require('./routes/helicopter');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/helicopter', helicoptertRouter);
+app.use('/helicopter', helicopterRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
